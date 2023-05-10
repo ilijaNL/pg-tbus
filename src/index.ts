37a9ee3b7398e7c299bbe186569071b7092d912a
ilija <276772+ilijaNL@users.noreply.github.com>
2023-05-10 14:30:39 +0200

    Fix expire in for tasks (#13)
    * Fix expire in for tasks
    
    * Fix lock
    
    * Added changeset

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import createTBus from './bus';
 
 export default createTBus;
-
-export { createTBus };
-
+export { Type } from '@sinclair/typebox';
+export * from './bus';
 export * from './sql';
 export * from './definitions';
