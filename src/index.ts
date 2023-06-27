435d1e0b67f4a889cafe2c77ff44bf7502ef96e7
ilijaNL <ilija.lisenkov@gmail.com>
2023-06-27 07:53:51 +0200

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import createTBus from './bus';
 
 export default createTBus;
-export { Type } from '@sinclair/typebox';
+export * from '@sinclair/typebox';
 export * from './bus';
 export * from './sql';
 export * from './definitions';
-export type { InsertTask, TaskDTO } from './messages';
+export type { InsertTask, TaskDTO, InsertEvent } from './messages';
